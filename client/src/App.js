82660ddd0b71af3821676dde54f8f24bdc501a37
Irfan <shaikh.irfan.oct16@gmail.com>
2023-12-03 01:21:05 +0530

    minor

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,3 @@
-// App.js
-
-import React from "react";
-import Navbar from "./components/Navbar";
-import HorizontalTimeline from "./components/HorizontalTimeline/Index";
-
-const App = () => {
-	const timelineEvents = [
-		{ title: "Event 1", description: "Description for Event 1" },
-		{ title: "Event 2", description: "Description for Event 2" },
-		{ title: "Event 3", description: "Description for Event 2" },
-		{ title: "Event 4", description: "Description for Event 2" },
-		// Add more events as needed
-	];
-
-	return (
-		<div className="m-2">
-			<Navbar />
-			<HorizontalTimeline events={timelineEvents} />
-		</div>
-	);
-};
 import LandingPage from "./components/LandingPage";
 import LoginPage from "./components/Loginpage";
 import Navbar from "./components/Navbar";
