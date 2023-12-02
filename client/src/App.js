9cb6c596f3418fcaa63fe6f7fef32bf8ddfe35bf
Irfan <shaikh.irfan.oct16@gmail.com>
2023-12-03 02:47:12 +0530

    events

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,7 @@
-import LandingPage from "./components/LandingPage";
-import LoginPage from "./components/Loginpage";
-//import Navbar from "./components/Navbar";
+import Event from "./components/Timeline/Event";
 
 function App() {
 	return (
-		<div className="container m-5 d-flex">
-			{/* <LoginPage /> */}
-			<Event width={100} height={300} padding={12} fill={"white"} bcolor={"black"} iscompleted={false} isActive={true} />
-			<div className="mx-4"></div>
-			<Event width={100} height={200} padding={20} fill={"white"} bcolor={"black"} iscompleted={false} isActive={true} />
-			<div className="mx-4"></div>
-			<Event width={200} height={200} padding={10} fill={"white"} bcolor={"black"} iscompleted={false} isActive={true} />
-			<div className="mx-4"></div>
-		</div>
 	);
 }
 
--- client/src/components/Timeline/Event.js
+++ client/src/components/Timeline/Event.js
@@ -12,13 +12,10 @@ const Event = ({ width, height, padding, fill, bcolor, iscompleted, isActive }) 
 
 	return (
 		<>
-			<div style={circleStyle}>
-				<svg className="" xmlns="http://www.w3.org/2000/svg" height={height - padding * 2} width={width - padding * 2} viewBox="0 0 512 512">
-					<path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM369 209L241 337c-9.4 9.4-24.6 9.4-33.9 0l-64-64c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l47 47L335 175c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9z" />
-				</svg>
-			</div>
+			<div style={circleStyle}></div>
 		</>
 	);
 };
 
+// const completed
 export default Event;
