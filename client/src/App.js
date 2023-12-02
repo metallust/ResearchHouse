be579bab154c0095b02352cee0b3eba1a37eb36b
Irfan <shaikh.irfan.oct16@gmail.com>
2023-12-03 04:32:43 +0530

    added routes and completed timeline

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,48 @@
 import LoginPage from "./components/Loginpage";
-//import Navbar from "./components/Navbar";
-
+import Signup from "./components/Signuppage";
+import LandingPage from "./components/LandingPage";
+import { Event, CompletedEvent, ActiveEvent } from "./components/Timeline/Event";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Timeline from "./components/Timeline/Index";
 
 function App() {
-  return (
-    <div >
-       <LoginPage />    
-    </div>
-  );
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/login" element={<LoginPage />} />
+				<Route path="/signup" element={<Signup />} />
+				<Route path="/" element={<LandingPage />} />
+				<Route
+					path="/irfan"
+					element={
+						<div className="container">
+							<Timeline />
+							<div className="row">
+								<div className="col p-5">
+									<Event width={50} height={20} />
+								</div>
+								<div className="col p-5">
+									<Event width={100} height={20} />
+								</div>
+								<div className="col p-5">
+									<CompletedEvent width={50} height={20} />
+								</div>
+								<div className="col p-5">
+									<CompletedEvent width={100} height={20} />
+								</div>
+								<div className="col p-5">
+									<ActiveEvent width={50} height={20} />
+								</div>
+								<div className="col p-5">
+									<ActiveEvent width={100} height={20} />
+								</div>
+							</div>
+						</div>
+					}
+				/>
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
