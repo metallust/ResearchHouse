95327f97adffe7d00946daf76339276cece1f8a8
thelifeisstrange <yogeshpk0123@gmail.com>
2023-12-08 02:14:44 +0530

    Table Component

---
+++ client/src/components/Table.js
@@ -0,0 +1,71 @@
+import React from "react";
+
+// limit of columns is 12
+const Table = (props) => {
+	const header = props.header;
+	const body = props.body;
+	return (
+		<div className='overflow-x-scroll container'>
+			<table className='table mt-2' style={{ whiteSpace: "nowrap" }}>
+				<thead
+					className='rounded-top'
+					style={{
+						backgroundColor: "#085F63",
+						border: "1px solid #085F63",
+					}}
+				>
+					<tr
+						className='rounded-top'
+						style={{
+							backgroundColor: "#085F63",
+							border: "1px solid #085F63",
+						}}
+					>
+						{header.map((element, id) => {
+							return (
+								<th
+									key={id}
+									scope='col'
+									className='text-center text-light bg-transparent'
+								>
+									{element}
+								</th>
+							);
+						})}
+					</tr>
+				</thead>
+				<tbody>
+					{body.map((apmt, id) => {
+						let rowcolor = apmt[0];
+						apmt = apmt.slice(1);
+						return (
+							<tr key={id}>
+								<th
+									scope='row'
+									className='text-center'
+									style={{ color: rowcolor }}
+								>
+									{id + 1}
+								</th>
+								{apmt.map((element, id2) => {
+									return (
+										<td
+											key={id2}
+											scope='row'
+											className='text-center'
+											style={{ color: rowcolor }}
+										>
+											{element}
+										</td>
+									);
+								})}
+							</tr>
+						);
+					})}
+				</tbody>
+			</table>
+		</div>
+	);
+};
+
+export default Table;
