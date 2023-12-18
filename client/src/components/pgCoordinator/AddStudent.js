26f232aa4bb569db6ddf968dc08c562375a88e54
Basit-Perampalli <apeerampalli@gmail.com>
2023-12-18 13:16:57 +0530

    minor

--- client/src/components/pgCoordinator/AddStudent.js
+++ client/src/components/pgCoordinator/AddStudent.js
@@ -4,48 +4,48 @@ import AddStdManually from './AddStdManually';
 import AddStdFile from './AddStdFile'
 
 const AddStudent = () => {
-  const [tab, setTab] = useState(0);
-  const handleSave =()=>{
+	const [tab, setTab] = useState(0);
+	const handleSave = () => {
 
-  }
-  return (
-    <div>
-      <div id='AddStudentModal' style={{borderRadius:"12px",height:"75vh",width:"55%",minWidth:"350px",}}>
-      <ul className='nav nav-tabs static'>
-						<li className='nav-item w-50'>
-							<a
-								className={`nav-link ${tab === 0 ? "active" : ""}`}
-								aria-current='page'
-								onClick={() => {
-									setTab(0);
-								}}
-								href='#'
-							>
-								<h4>Add Manually</h4>
-							</a>
-						</li>
-						<li className='nav-item w-50'>
-							<a
-								className={`nav-link ${tab === 1 ? "active" : ""}`}
-								aria-current='page'
-								onClick={() => {
-									setTab(1);
-								}}
-								href='#'
-							>
-							  <h4>	Add Using CSV,XL File</h4>
-							</a>
-						</li>
-					</ul>
-          <div className='container mt-3' style={{height:"80%",overflowX:"scroll"}}>
-            {tab === 0 ? <AddStdManually/> : <AddStdFile/>}
-          </div>
-            <div className='container d-flex justify-content-between '> 
-              <div className="btn btn-dark" onClick={handleSave}>Send Credentials and Save</div>
-            </div>
-      </div>
-    </div>
-  )
+	}
+	return (
+		<div>
+			<div id='AddStudentModal' style={{ borderRadius: "12px", width: "55%", }}>
+				<ul className='nav nav-tabs static'>
+					<li className='nav-item w-50'>
+						<a
+							className={`nav-link ${tab === 0 ? "active" : ""}`}
+							aria-current='page'
+							onClick={() => {
+								setTab(0);
+							}}
+							href='#'
+						>
+							<h4>Add Manually</h4>
+						</a>
+					</li>
+					<li className='nav-item w-50'>
+						<a
+							className={`nav-link ${tab === 1 ? "active" : ""}`}
+							aria-current='page'
+							onClick={() => {
+								setTab(1);
+							}}
+							href='#'
+						>
+							<h4>Add Using XL File</h4>
+						</a>
+					</li>
+				</ul>
+				<div className='container mt-3' style={{ height: "80%", overflowX: "scroll" }}>
+					{tab === 0 ? <AddStdManually /> : <AddStdFile />}
+				</div>
+				<div className='container d-flex justify-content-between '>
+					<div className="btn btn-dark" onClick={handleSave}>Send Credentials and Save</div>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default AddStudent
