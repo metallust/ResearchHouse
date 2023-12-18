172135c986904bed6e0e58044d617decabd8c777
thelifeisstrange <yogeshpk0123@gmail.com>
2023-12-18 21:00:43 +0530

    right sidebar

--- client/src/components/pgCoordinator/PgCoordinatorDashboard.js
+++ client/src/components/pgCoordinator/PgCoordinatorDashboard.js
@@ -1,10 +1,11 @@
 import React from "react";
 // import StudentSidebar from "../student/StudentSidebar";
 import PgCoordinatorSidebar from "./PgCoordinatorSidebar";
+import PgCoordinatorRightSidebar from "./PgCoordinatorRightSidebar";
 
 const PgCoordinatorDashboard = () => {
 	return (
-		<div className='d-flex'>
+		<div className='d-flex justify-content-between'>
 			<div
 				style={{
 					backgroundColor: "transparent",
@@ -14,6 +15,17 @@ const PgCoordinatorDashboard = () => {
 			>
 				<div>
 					<PgCoordinatorSidebar />
+				</div>
+			</div>
+			<div
+				style={{
+					backgroundColor: "transparent",
+					minHeight: "100vh",
+					width: "21%",
+				}}
+			>
+				<div>
+					<PgCoordinatorRightSidebar />
 				</div>
 			</div>
 		</div>
