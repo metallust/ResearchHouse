02006dbe0f6f386e28bb6cae7f002215d5ff375f
thelifeisstrange <yogeshpk0123@gmail.com>
2023-12-20 08:47:53 +0530

    analytics final

--- client/src/components/pgCoordinator/Pacman.js
+++ client/src/components/pgCoordinator/Pacman.js
@@ -2,33 +2,33 @@ import React from "react";
 import { Chart } from "react-google-charts";
 
 export const data = [
-  ["Language", "Speakers (in millions)"],
-  ["Assamese", 13],
-  ["Bengali", 83],
+	["Language", "Speakers (in millions)"],
+	["Assamese", 13],
+	["Bengali", 83],
 ];
 
 export const options = {
-  title: "Indian Language Use",
-  legend: "none",
-  pieSliceText: "label",
-  slices: {
-    4: { offset: 0.2 },
-    12: { offset: 0.3 },
-    14: { offset: 0.4 },
-    15: { offset: 0.5 },
-  },
+	title: "Domains Choosen By Student",
+	legend: "none",
+	pieSliceText: "label",
+	slices: {
+		4: { offset: 0.2 },
+		12: { offset: 0.3 },
+		14: { offset: 0.4 },
+		15: { offset: 0.5 },
+	},
 };
 
 function Pacman() {
-  return (
-    <Chart
-      chartType="PieChart"
-      data={data}
-      options={options}
-      width={"100%"}
-      height={"100px"}
-    />
-  );
+	return (
+		<Chart
+			chartType='PieChart'
+			data={data}
+			options={options}
+			width={"100%"}
+			height={"100px"}
+		/>
+	);
 }
 
 export default Pacman;
--- client/src/components/pgCoordinator/PgAnalytics.js
+++ client/src/components/pgCoordinator/PgAnalytics.js
@@ -4,7 +4,7 @@ import Pacman from "./Pacman";
 
 const PgAnalytics = () => {
 	const [tab, setTab] = useState(0);
-	const handleSave = () => { };
+	const handleSave = () => {};
 
 	const guideDomains = [
 		"Machine Learning",
@@ -18,33 +18,51 @@ const PgAnalytics = () => {
 		backgroundColor: "#E1F8FF",
 		height: "100%",
 		width: "32%",
-		border:"none",
-		borderRadius:"10px",
+		border: "none",
+		borderRadius: "10px",
 		boxShadow: "0px 3px 4px 0px rgba(0,0,0,0.4)",
-		padding:"9px 13px"
-	}
+		padding: "9px 13px",
+	};
 	return (
 		<div>
-			<div className="d-flex justify-content-between" style={{ height: "25vh",margin:"10px 2rem" }}>
+			<div
+				className='d-flex justify-content-between'
+				style={{
+					height: "28vh",
+					margin: "10px 2rem",
+					maxHeight: "180px",
+				}}
+			>
 				<div style={card}>
-					<div className="h5">Student/Guide Ratio</div>
-					<div className="d-flex align-items-baseline">
-						<div style={{fontSize:"4rem",fontWeight:"600"}}>5.6</div>
-						<div className="ms-2">1.4 less than Maximum</div>
+					<div className='h5'>Student/Guide Ratio</div>
+					<div className='d-flex align-items-baseline'>
+						<div style={{ fontSize: "2.5rem", fontWeight: "600" }}>
+							5.6
+						</div>
+						<div className='ms-3'>1.4 less than Maximum</div>
 					</div>
-					<div className="">Recommended Maximum 7</div>
+					<div className='mt-2'>Recommended Maximum 7</div>
 				</div>
 				<div style={card}>
-					<div className="h5">Student Allocation</div>
-					<div className="d-flex align-items-baseline "><h3 className="me-3">43</h3> Students Allocated</div>
-					<div className="d-flex align-items-baseline "><h3 className="me-3">12</h3> Students Remaining</div>
-					<div className="btn d-flex justify-content-center" style={{backgroundColor:"#004257",color:"white"}}>Allocate</div>
+					<div className='h5'>Student Allocation</div>
+					<div className='d-flex align-items-baseline '>
+						<h3 className='me-3'>43</h3> Students Allocated
+					</div>
+					<div className='d-flex align-items-baseline '>
+						<h3 className='me-3'>12</h3> Students Remaining
+					</div>
+					<div
+						className='btn d-flex justify-content-center'
+						style={{ backgroundColor: "#004257", color: "white" }}
+					>
+						Allocate
+					</div>
 				</div>
 				<div style={card}>
-				<div className="h5">Submission in Deadline</div>
-				<div>
-					<Pacman/>
-				</div>
+					<div className='h5'>Submission in Deadline</div>
+					<div>
+						<Pacman />
+					</div>
 				</div>
 			</div>
 			<div>
